8102c3cbadaba270ef6bc2e38c5bffd8f1c52298
Kasim Nurlgn <kasimnurlgn9@gmail.com>
2024-11-28 09:01:03 +0300

    Some fixes

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5500/api/",
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem("token")}`,
-  },
+  baseURL: "https://evangadiforumbackend-3-adp6.onrender.com/api/",
+  // headers: {
+  //   Authorization: `Bearer ${localStorage.getItem("token")}`,
+  // },
 });
 
 export default instance;
